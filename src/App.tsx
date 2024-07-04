6714d5c78abf001714bc3d477e2ae1877afa2d96
dinhtiendat190196 <dinhtiendat1901@gmail.com>
2024-07-04 15:20:18 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,7 @@ import HomePage from "./page/HomePage.tsx";
 
 export default function App() {
     return <MantineProvider>
-        <AppShell header={{height: 150}} navbar={{
-            width: 300,
-            breakpoint: 'sm'
-        }} padding="md">
-            <AppShell.Header p="md">
-            </AppShell.Header>
-            <AppShell.Navbar p="md">
-            </AppShell.Navbar>
+        <AppShell padding="xl" pt={100}>
             <AppShell.Main>
                 <HomePage/>
             </AppShell.Main>
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,33 +1,48 @@
-import {Button, Group, Paper, Stack, Text, TextInput} from "@mantine/core";
+import {ActionIcon, Button, CopyButton, Flex, Group, Paper, rem, Stack, TextInput, Tooltip} from "@mantine/core";
 import {useRef, useState} from "react";
 import {invoke} from "@tauri-apps/api";
 import Markdown from "react-markdown";
+import {IconCheck, IconCopy} from '@tabler/icons-react';
 
 export default function HomePage() {
     const [result, setResult] = useState('');
+    const [textToCopy, setTextToCopy] = useState('');
     const input = useRef<HTMLInputElement>(null);
 
+
     async function handleClickRender() {
-        console.log(input.current?.value)
         const folderStructure = await invoke('get_folder_structure', {path: input.current?.value});
         setResult('```\n' + folderStructure as string + '\n```')
+        setTextToCopy('\nI will describe my project to you. Confirm with me that you clearly understand it.\nJust answer yes or no. I don\'t need any more information.' + folderStructure as string)
     }
 
     return <Stack>
-        <Group>
-            <TextInput placeholder="Text" ref={input}/>
-            <Button onClick={handleClickRender}>Render</Button>
+        <Group justify="center">
+            <TextInput placeholder="Enter your path" ref={input} variant="filled" w={700} size="lg" radius='xl'/>
+            <Button onClick={handleClickRender} radius='xl' variant="gradient"
+                    gradient={{from: 'violet', to: 'pink', deg: 91}} size='lg'>Render</Button>
         </Group>
-        <Paper shadow="xs" radius="lg" p="xl" withBorder>
-            <Text>
-                I will describe my project to you. Confirm with me that you clearly understand it.
-            </Text>
-            <Text>
-                Just answer yes or no. I don't need any more information.
-            </Text>
-            <Markdown>
-                {result}
-            </Markdown>
+        <Paper shadow="xs" radius="lg" p="md" withBorder mt={30}>
+            <Flex justify="space-between">
+                <Stack>
+                    <Markdown>
+                        {result}
+                    </Markdown>
+                </Stack>
+                <CopyButton value={textToCopy} timeout={2000}>
+                    {({copied, copy}) => (
+                        <Tooltip label={copied ? 'Copied' : 'Copy'} withArrow position="right">
+                            <ActionIcon color={copied ? 'teal' : 'gray'} variant="subtle" onClick={copy}>
+                                {copied ? (
+                                    <IconCheck style={{width: rem(16)}}/>
+                                ) : (
+                                    <IconCopy style={{width: rem(16)}}/>
+                                )}
+                            </ActionIcon>
+                        </Tooltip>
+                    )}
+                </CopyButton>
+            </Flex>
         </Paper>
     </Stack>
 }
